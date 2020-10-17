7aa808c6a0ed006018ae27326044ad88285e187e
Bobby Neal <rsneal01@gmail.com>
2020-10-17 16:26:35 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
   
+  def say_that_thing_you_say
+    puts "#{self.name} always says: #{self.catchphrase}"
+  end
+  
+  
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
   
+  has_many :actors, through: :characters
+  belongs_to :network
+  
+  def actors_list
+    self.characters.actors.map do |actor|
+      actor.full_name
+    end
+  end
+  
+  
+  
 end
