521dc3340ba01a8f7eae17a65e0776b6261a28d5
Bobby Neal <rsneal01@gmail.com>
2020-10-17 16:57:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
   
   def actors_list
-    self.characters.actor.map do |actor|
+    self.actors.map do |actor|
       actor.full_name
     end
   end
